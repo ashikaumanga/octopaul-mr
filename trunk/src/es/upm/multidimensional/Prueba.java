d003d11cbe6a8b8c4897067750cf981ec9600192
fcojavibr <fcojavibr@6cb63be0-b67f-d87e-e144-2da257a1f9d5>
2010-08-27 13:15:15 +0000

    New constructors added to facilitate the generation of the recommender system New tests added

--- trunk/src/es/upm/multidimensional/Prueba.java
+++ trunk/src/es/upm/multidimensional/Prueba.java
@@ -4,13 +4,15 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Random;
 
+import org.apache.mahout.cf.taste.common.TasteException;
+
 
 public class Prueba {
 	
-	public static HashMap<String, HashMap<Long, HashMap<Long, Float>>> createData(String[] dimensions,
-																				  int numUsers, int numItems, 
-																				  int ratingsPerUser){
-		HashMap<String, HashMap<Long, HashMap<Long, Float>>> hashMap = new HashMap<String, HashMap<Long, HashMap<Long, Float>>>();
+	public static HashMap<String, Object> createData(String[] dimensions,
+													 int numUsers, int numItems, 
+													 int ratingsPerUser){
+		HashMap<String, Object> hashMap = new HashMap<String, Object>();
 		for(int k=0; k<dimensions.length; k++){
 			HashMap<Long, HashMap<Long,Float>> hashMapByDim = new HashMap<Long, HashMap<Long,Float>>();
 			Random randomGenerator = new Random();
@@ -30,28 +32,62 @@ public class Prueba {
 	
 	public static void main(String[] args) throws Exception{
 		
-		int numUsers=10000;
+		int numUsers=1000;
 		int numItems=500;
 		int ratingsPerUser=50;
-		int myUser=90;
+		int myUsers[]={1}; //,90,190,400};
 		
 		String[] dimensions = {"dim_1","dim_2","dim_3"};
+		
+		HashMap<String, Object>  dataMap = createData(dimensions, numUsers,numItems,ratingsPerUser);
+		Object dataArray[] = new Object[dataMap.size()];
+		int k = 0;
+		for(Object data : dataMap.values())
+			dataArray[k++] = data;
+		
 		double[] ponderations = {0.3, 0.4, 0.3};
-		String[] algorithms = {RecommendationGenerator.PEARSON,RecommendationGenerator.PEARSON, RecommendationGenerator.PEARSON};
+		HashMap<String, Double> ponderationMap = new HashMap<String, Double>();
+		for (k = 0; k < ponderations.length; k++)
+			ponderationMap.put(dimensions[k], ponderations[k]);
 		
-		HashMap<String,HashMap<Long, HashMap<Long, Float>>>  hashMap = createData(dimensions, numUsers,numItems,ratingsPerUser);
+		String[] algorithms = {RecommendationGenerator.PEARSON,
+				RecommendationGenerator.PEARSON, RecommendationGenerator.PEARSON};
+		HashMap<String, String> algorithmMap = new HashMap<String, String>();
+		for (k = 0; k < algorithms.length; k++)
+			algorithmMap.put(dimensions[k], algorithms[k]);
 		
-		RecommendationGenerator generator = new RecommendationGenerator(hashMap, ponderations, algorithms);
-		
-		//generator.showInfo();
-		HashMap<Long,Float> recommendations = generator.getRecommendations(myUser, 5);
-		Iterator<Long> recIterator = recommendations.keySet().iterator();
-		
-		while(recIterator.hasNext()){
-			Long key = (Long) recIterator.next();
-			Float value = recommendations.get(key);
-			System.out.println("Item number: "+key+", preference = "+value+".");				
-		}		
-			
+		System.out.println("--------------------------- Test 1 ----------------------------------");
+		RecommendationGenerator generator = new RecommendationGenerator(dataMap, ponderations, algorithms);
+		print(generator, myUsers);
+		System.out.println("--------------------------- Test 2 ----------------------------------");
+		generator = new RecommendationGenerator(dataMap);
+		print(generator, myUsers);
+		System.out.println("--------------------------- Test 3 ----------------------------------");
+		generator = new RecommendationGenerator(dimensions, dataArray);
+		print(generator, myUsers);
+		System.out.println("--------------------------- Test 4 ----------------------------------");
+		String newAlgorithms[] = {RecommendationGenerator.TANIMOTO,
+				RecommendationGenerator.EUCLIDIAN, RecommendationGenerator.COSINE};
+		generator = new RecommendationGenerator(dimensions, dataArray, ponderations, newAlgorithms);
+		print(generator, myUsers);
+		System.out.println("--------------------------- Test 5 ----------------------------------");
+		generator = new RecommendationGenerator(dataMap, ponderationMap, algorithmMap);
+		print(generator, myUsers);
+					
 	}
+	
+	public static void print(RecommendationGenerator generator, int myUsers[]) throws TasteException {
+		generator.showInfo();
+		for(int myUser : myUsers) {
+			HashMap<Long,Float> recommendations = generator.getRecommendations(myUser, 5);
+			Iterator<Long> recIterator = recommendations.keySet().iterator();
+			
+			while(recIterator.hasNext()){
+				Long key = (Long) recIterator.next();
+				Float value = recommendations.get(key);
+				System.out.println("Item number: "+key+", preference = "+value+".");				
+			}
+		}
+	}
+	
 }
--- trunk/src/es/upm/multidimensional/RecommendationGenerator.java
+++ trunk/src/es/upm/multidimensional/RecommendationGenerator.java
@@ -43,8 +43,9 @@ public class RecommendationGenerator {
 	private final Logger log = LoggerFactory.getLogger(RecommendationGenerator.class);	
 	private static final long MIN_RELOAD_INTERVAL_MS = 60 * 1000L; // 1 minute?	
 	
-	// HashMaps, they are all indexed by an String object containing the name of the dimension. 
-	private HashMap<String, HashMap<Long, HashMap<Long, Float>>> hashMapData;
+	// HashMaps, they are all indexed by an String object containing the name of the dimension.
+	private HashMap<String, Object> dimensionData;
+	//private HashMap<String, HashMap<Long, HashMap<Long, Float>>> hashMapData;
 	private HashMap<String, Double> ponderations;
 	private HashMap<String, String> similarityAlgorithms;
 	private HashMap<String, Integer> similarityIntMap;
@@ -58,32 +59,120 @@ public class RecommendationGenerator {
 	// Recommender instance:
 	public Recommender recommender;
 	
+	
 	/**
 	 * Contructor for RecommendationGenerator
-	 * @param hashMapData : database for the recommender system.
+	 * @param mapData : array of dimension database for the recommender system.
+	 * dimension ponderation: uniform ponderation of 1.0 for each dimension
+	 * dimension similarity algorithm: PEARSON default algorithm for each dimension 
+	 * @throws Exception
+	 */
+	public RecommendationGenerator(	String[] dimensions,
+									Object[] mapData) throws Exception{
+		// Verify the same dimensions length of items 
+		if(dimensions.length != mapData.length)
+			throw new Exception("We need to have the same number of dimensions for every" +
+					" input argument of the constructor method.");
+		
+		// Identify the dimensions.
+		this.dimensions = dimensions;
+		
+		HashMap<String, Object> hashMapData = new HashMap<String, Object>();
+		HashMap<String, Double> ponderations=new HashMap<String, Double>();
+		HashMap<String, String> similarityAlgorithms=new HashMap<String, String>();
+		for(int i = 0; i < dimensions.length; i++)	{
+			hashMapData.put(dimensions[i], mapData[i]);
+			ponderations.put(dimensions[i], 1.0);
+			similarityAlgorithms.put(dimensions[i], RecommendationGenerator.PEARSON);
+		}		
+
+		configure(hashMapData, ponderations, similarityAlgorithms);
+		refresh();		
+	}
+	
+	/**
+	 * Contructor for RecommendationGenerator
+	 * @param mapData : array of dimension database for the recommender system.
+	 * dimension ponderation: uniform ponderation of 1.0 for each dimension
+	 * dimension similarity algorithm: PEARSON default algorithm for each dimension 
+	 * @throws Exception
+	 */
+	public RecommendationGenerator(	HashMap<String,Object> dimensionData) throws Exception{
+		
+		// Identify the dimensions.
+		this.dimensions = new String[dimensionData.size()];
+		int k = 0;
+		HashMap<String, Double> ponderations=new HashMap<String, Double>();
+		HashMap<String, String> similarityAlgorithms=new HashMap<String, String>();
+		for(String dimension : dimensionData.keySet())	{
+			dimensions[k++] = dimension;
+			ponderations.put(dimension, 1.0);
+			similarityAlgorithms.put(dimension, RecommendationGenerator.PEARSON);
+		}		
+
+		configure(dimensionData, ponderations, similarityAlgorithms);
+		refresh();		
+	}
+	
+	/**
+	 * Contructor for RecommendationGenerator
+	 * @param mapData : array of dimension database for the recommender system.
 	 * @param dimensionPonderations : array of Double objects that weights each dimension.
 	 * @param dimensionSimAlgorithms : array of String objects that specify which similarity algorithm we want to use in ecah dimension.
 	 * @throws Exception
 	 */
-	public RecommendationGenerator(	HashMap<String, HashMap<Long, HashMap<Long, Float>>> hashMapData,
+	public RecommendationGenerator(	String[] dimensions, Object[] mapData,
 									double[] dimensionPonderations,
 									String[] dimensionSimAlgorithms) throws Exception{
+		// Verify the same dimensions length of items 
+		if(dimensions.length != mapData.length || dimensions.length != dimensionPonderations.length
+				|| dimensions.length != dimensionSimAlgorithms.length)
+			throw new Exception("We need to have the same number of dimensions for every" +
+					" input argument of the constructor method.");
+		
+		// Identify the dimensions.
+		this.dimensions = dimensions;
+		
+		HashMap<String, Object> hashMapData = new HashMap<String, Object>();
+		HashMap<String, Double> ponderations=new HashMap<String, Double>();
+		HashMap<String, String> similarityAlgorithms=new HashMap<String, String>();
+		for(int i = 0; i < dimensions.length; i++)	{
+			hashMapData.put(dimensions[i], mapData[i]);
+			ponderations.put(dimensions[i], dimensionPonderations[i]);
+			similarityAlgorithms.put(dimensions[i],dimensionSimAlgorithms[i]);
+		}		
+
+		configure(hashMapData, ponderations, similarityAlgorithms);
+		refresh();		
+	}
+	
+	
+	/**
+	 * Contructor for RecommendationGenerator
+	 * @param hashMapData : map of dimension database for the recommender system.
+	 * @param dimensionPonderations : array of Double objects that weights each dimension.
+	 * @param dimensionSimAlgorithms : array of String objects that specify which similarity algorithm we want to use in ecah dimension.
+	 * @throws Exception
+	 */
+	public RecommendationGenerator(	HashMap<String, Object> hashMapData,
+									double[] dimensionPonderations,
+									String[] dimensionSimAlgorithms) throws Exception{
+		if(dimensionPonderations.length != hashMapData.size() || 
+				dimensionPonderations.length != dimensionSimAlgorithms.length)
+			throw new Exception("We need to have the same number of dimensions for every" +
+					" input argument of the constructor method.");
 		
 		// Identify the dimensions.			
-		String[] arrayDim=new String[hashMapData.size()];
+		dimensions=new String[hashMapData.size()];
 		Iterator<String> dimIterator = hashMapData.keySet().iterator();
 		int k=0;
 		while(dimIterator.hasNext()){			
-			String dim=dimIterator.next();
-			arrayDim[k]=dim;
-			k++;
-		}
-		this.dimensions=arrayDim;
+			dimensions[k++] = dimIterator.next();
+		}		
 		
 //		this.dimensions=(String[]) hashMapData.keySet().toArray();		
 		HashMap<String, Double> ponderations=new HashMap<String, Double>();
-		HashMap<String, String> similarityAlgorithms=new HashMap<String, String>();
-		
+		HashMap<String, String> similarityAlgorithms=new HashMap<String, String>();		
 		for(int i=0; i<dimensions.length; i++){
 			ponderations.put(dimensions[i], dimensionPonderations[i]);
 			similarityAlgorithms.put(dimensions[i],dimensionSimAlgorithms[i]);
@@ -99,42 +188,57 @@ public class RecommendationGenerator {
 	 * @param similarityAlgorithms : HashMap of String objects indexed by String objects (every String object stores the name of a dimension).
 	 * @throws Exception
 	 */
-	public RecommendationGenerator(	HashMap<String, HashMap<Long, HashMap<Long, Float>>> hashMapData,
+	public RecommendationGenerator(	HashMap<String, Object> hashMapData,
 									HashMap<String, Double> ponderations,
 									HashMap<String, String> similarityAlgorithms) throws Exception{
 		configure(hashMapData, ponderations, similarityAlgorithms);
 		refresh();		
 	}
 	
-	private void configure 	(HashMap<String, HashMap<Long, HashMap<Long, Float>>> hashMapData,
+	private void configure 	(HashMap<String, Object> mapData,
 			HashMap<String, Double> ponderations,HashMap<String, String> similarityAlgorithms) 
-			throws Exception {
+			throws Exception {		
 		log.info("Starting the recommendation engine.");		
 		// Check that the number of dimensions is correct.
-		if( (hashMapData.size()!=ponderations.size() || similarityAlgorithms.size()!=ponderations.size() ))
-				throw new Exception("We need to have the same number of dimensions for every input argument of the constructor method.");
+		if( (mapData.size()!=ponderations.size() || similarityAlgorithms.size()!=ponderations.size() ))
+				throw new Exception("We need to have the same number of dimensions for every " +
+						"input argument of the constructor method.");
+		//Check that mapData values corresponds to DataModel or HashMap<Long, HashMap<Long, Float>> instances
+		for(Object data : mapData.values()) {
+			if(!(data instanceof DataModel)) {
+				if(!(data instanceof HashMap)) {
+					throw new Exception("The dimensional data have to be instances of DataModel " +
+							"or HashMap<Long, HashMap<Long, Float>>");
+				}
+			}
+		}
 		
 		// Identify the dimensions.
-		String[] arrayDim=new String[hashMapData.size()];
-		Iterator<String> dimIterator = hashMapData.keySet().iterator();
-		int k=0;
-		while(dimIterator.hasNext()){			
-			String dim=dimIterator.next();
-			arrayDim[k]=dim;
-			k++;
+		if(dimensions == null) {
+			dimensions = new String[mapData.size()];
+			Iterator<String> dimIterator = mapData.keySet().iterator();
+			int k=0;
+			while(dimIterator.hasNext()){			
+				String dim = dimIterator.next();
+				dimensions[k++] = dim;				
+				if(!ponderations.containsKey(dim))
+					 throw new Exception("Different dimension names between hasMapData and ponderations.");
+				if(!similarityAlgorithms.containsKey(dim))
+					 throw new Exception("Different dimension names between hasMapData and similarity algorithm.");
+			}					
 		}
-		this.dimensions=arrayDim;		
-				
+		
 		// Validates the names of the chosen algorithms
 		for(int i=0; i<similarityAlgorithms.size(); i++){
 			String a = similarityAlgorithms.get(dimensions[i]);
 			if((a!=EUCLIDIAN)&&(a!=LOGLIKELIHOOD)&&(a!=PEARSON)&&(a!=SPEARMAN)&&(a!=TANIMOTO)&&(a!=COSINE)){
-				throw new Exception("\""+a+"\""+" is not a valid name for a similarity algorithm for the dimension: \""+dimensions[i]+"\".");
+				throw new Exception("\""+a+"\""+" is not a valid name for a similarity " +
+						"algorithm for the dimension: \""+dimensions[i]+"\".");
 			}
 		}
 		log.info("The dimensions of the input data seem to be correct");
 		// Initialization of variables.
-		this.hashMapData=hashMapData;
+		this.dimensionData=mapData;
 		this.ponderations=ponderations;
 		this.similarityAlgorithms=similarityAlgorithms;
 		this.similarityIntMap= new HashMap<String, Integer>();
@@ -164,15 +268,22 @@ public class RecommendationGenerator {
 			for(int i=0; i<dimensions.length;i++ ){
 				String dimension = dimensions[i];
 				log.info("Regarding the "+"\""+" dimension:");
-				HashMap<Long, HashMap<Long, Float>> auxHashMap = hashMapData.get(dimension);
-				checkHashMap(auxHashMap);
-				log.info("Creating DataModel instance for the"+"\""+dimension+"\""+" dimension.");
-				DataModel dataModel =createDataModel(auxHashMap);
-				dataModels.put(dimension, dataModel);
-				log.info("Creating UserSimilarity instance for the"+"\""+dimension+"\""+" dimension.");
+				DataModel dataModel;
+				if(dimensionData.get(dimension) instanceof DataModel) {
+					dataModel = (DataModel) dimensionData.get(dimension);
+					dataModels.put(dimension, dataModel);
+				} else {
+					HashMap<Long, HashMap<Long, Float>> auxHashMap = 
+						(HashMap<Long, HashMap<Long, Float>>) dimensionData.get(dimension);
+					checkHashMap(auxHashMap);					
+					log.info("Creating DataModel instance for the "+"\""+dimension+"\""+" dimension.");
+					dataModel = createDataModel(auxHashMap);
+					dataModels.put(dimension, dataModel);
+				}
+				log.info("Creating UserSimilarity instance for the "+"\""+dimension+"\""+" dimension.");
 				UserSimilarity userSimilarity = createUserSimilarity(similarityAlgorithms.get(dimension),dataModel);
 				similarities.put(dimension,userSimilarity);
-				log.info("Creating UserNeighborhood instance for the"+"\""+dimension+"\""+" dimension.");
+				log.info("Creating UserNeighborhood instance for the "+"\""+dimension+"\""+" dimension.");
 				UserNeighborhood userNeighborhood = new NearestNUserNeighborhood(neighbors,userSimilarity,dataModel);
 				neighborhoods.put(dimension,userNeighborhood);			
 				log.info("UserNeighborhood instance created successfully.");
@@ -218,17 +329,21 @@ public class RecommendationGenerator {
 	 * @param itemID : item for which we want to remove a rating.
 	 */
 	public void removeRating(String dim, long userID, long itemID){
-		HashMap<Long,HashMap<Long,Float>> hashMapByDimension = (HashMap<Long,HashMap<Long,Float>>) hashMapData.get(dim);
-		if(hashMapByDimension==null){//
+		if(dimensionData.containsKey(dim)){//
 			throw new IllegalArgumentException("This dimension does not exist.");
 		}
+		Object map = dimensionData.get(dim);
+		if(! (map instanceof HashMap)) {
+			throw new IllegalArgumentException("This dimension uses Data Model directly and not hash Map");
+		}
+		HashMap<Long,HashMap<Long,Float>> hashMapByDimension = (HashMap<Long,HashMap<Long,Float>>) map;		
 		HashMap<Long,Float> hashMapByUid = (HashMap<Long,Float>) hashMapByDimension.get(userID);
 		if(hashMapByUid==null){//
 			throw new IllegalArgumentException("This user has not rated this dimension.");
 		}
 		hashMapByUid.remove(itemID);
-		hashMapByDimension.put(userID, hashMapByUid);
-		hashMapData.put(dim, hashMapByDimension);
+		//hashMapByDimension.put(userID, hashMapByUid);
+		//hashMapData.put(dim, hashMapByDimension);
 		this.refreshed=false;
 		log.info("You have removed the rating of item number "+itemID+" for user number "+userID+" at the dimension: \""+dim+"\".");
 		log.info("You may refresh now.");
@@ -242,21 +357,25 @@ public class RecommendationGenerator {
 	 * @param value : value of the rating.
 	 */
 	public void putRating(String dim, long userID, long itemID, float value){
-		HashMap<Long,HashMap<Long,Float>> hashMapByDimension = (HashMap<Long,HashMap<Long,Float>>) hashMapData.get(dim);
-		if(hashMapByDimension==null){//
+		if(dimensionData.containsKey(dim)){//
 			throw new IllegalArgumentException("This dimension does not exist.");
 		}
+		Object map = dimensionData.get(dim);
+		if(! (map instanceof HashMap)) {
+			throw new IllegalArgumentException("This dimension uses Data Model directly and not hash Map");
+		}
+		HashMap<Long,HashMap<Long,Float>> hashMapByDimension = (HashMap<Long,HashMap<Long,Float>>) map;
 		HashMap<Long,Float> hashMapByUid = (HashMap<Long,Float>) hashMapByDimension.get(userID);
 		if(hashMapByUid==null){//new user
 			HashMap<Long,Float> newUserHashMap = new HashMap<Long,Float>();
 			newUserHashMap.put(itemID, value);
 			hashMapByDimension.put(userID, newUserHashMap);
-			hashMapData.put(dim, hashMapByDimension);
+			//hashMapData.put(dim, hashMapByDimension);
 			return;
 		}
 		hashMapByUid.put(itemID,value);
-		hashMapByDimension.put(userID,hashMapByUid);
-		hashMapData.put(dim, hashMapByDimension);
+		//hashMapByDimension.put(userID,hashMapByUid);
+		//hashMapData.put(dim, hashMapByDimension);
 		this.refreshed=false;
 		log.info("You have put the rating of "+value+" to item number "+itemID+" for user number "+userID+" at the dimension: \""+dim+"\".");
 		log.info("You may refresh now.");
@@ -268,13 +387,13 @@ public class RecommendationGenerator {
 	 * @param similarityAlgorithm : similarity algorithm we want to assign to the given dimension.
 	 */
 	public void setSimilarity(String dimension, String similarityAlgorithm){	
-		if (hashMapData.keySet().contains(dimension)){
+		if (similarityAlgorithms.containsKey(dimension)){
 			similarityAlgorithms.put(dimension, similarityAlgorithm);
 			this.refreshed=false;
 			log.info("You have set \""+similarityAlgorithm+"\" algorithm for \""+dimension+"\" dimension.");
 			log.info("You may refresh now.");
-		}
-
+		} else
+			throw new IllegalArgumentException("This dimension does not exist.");
 	}
 	
 	/**
@@ -294,7 +413,8 @@ public class RecommendationGenerator {
 		System.out.println("*********************************************************************************************");
 		System.out.println("THESE ARE THE VALUES OF THE RECOMMENDER SYSTEM:");
 		for(int i=0; i<dimensions.length;i++ ){
-			System.out.println("You have set \""+similarityAlgorithms.get(dimensions[i])+"\" similarity calculation algorithm for \""+dimensions[i]+"\" dimension.");
+			System.out.println("You have set \""+similarityAlgorithms.get(dimensions[i]) +
+					"\" similarity calculation algorithm for \""+dimensions[i]+"\" dimension.");
 		}		
 		System.out.println("The recommender is based on a "+neighbors+"-Nearest Neighbors Algorithm.");
 		if(!refreshed)
@@ -374,13 +494,27 @@ public class RecommendationGenerator {
 	private UserSimilarity createUserSimilarity(String similarity,DataModel dataModel) throws TasteException {
 		int sim=similarityIntMap.get(similarity);
 		switch(sim){
-		case 1:  log.info("Euclidean distance UserSimilarity instance created successfully."); return new EuclideanDistanceSimilarity(dataModel);
-		case 2:  log.info("Log Likelihood UserSimilarity instance created successfully.");return new LogLikelihoodSimilarity(dataModel); 
-		case 3:  log.info("Pearson correlation UserSimilarity instance created successfully.");return new PearsonCorrelationSimilarity(dataModel);
-		case 4:  log.info("Spearman correlation UserSimilarity instance created successfully.");return new SpearmanCorrelationSimilarity(dataModel);
-		case 5:  log.info("Tanimoto coefficient UserSimilarity instance created successfully.");return new TanimotoCoefficientSimilarity(dataModel); 
-		case 6:  log.info("Uncentered cosine UserSimilarity instance created successfully.");return new UncenteredCosineSimilarity(dataModel);
-		default: log.warn("Default value: Pearson correlation UserSimilarity instance returned.");return new PearsonCorrelationSimilarity(dataModel);
+		case 1:  
+			log.info("Euclidean distance UserSimilarity instance created successfully."); 
+			return new EuclideanDistanceSimilarity(dataModel);
+		case 2:  
+			log.info("Log Likelihood UserSimilarity instance created successfully.");
+			return new LogLikelihoodSimilarity(dataModel); 
+		case 3:  
+			log.info("Pearson correlation UserSimilarity instance created successfully.");
+			return new PearsonCorrelationSimilarity(dataModel);
+		case 4:  
+			log.info("Spearman correlation UserSimilarity instance created successfully.");
+			return new SpearmanCorrelationSimilarity(dataModel);
+		case 5:  
+			log.info("Tanimoto coefficient UserSimilarity instance created successfully.");
+			return new TanimotoCoefficientSimilarity(dataModel); 
+		case 6:  
+			log.info("Uncentered cosine UserSimilarity instance created successfully.");
+			return new UncenteredCosineSimilarity(dataModel);
+		default: 
+			log.warn("Default value: Pearson correlation UserSimilarity instance returned.");
+			return new PearsonCorrelationSimilarity(dataModel);
 		}
 	}
 	
